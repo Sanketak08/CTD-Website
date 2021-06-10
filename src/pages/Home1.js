d633ebc5c26d8ff2f016d1b6b95d790795796736
Sanket Kulkarni <sakulkarni0801@gmail.com>
2021-06-10 18:56:09 +0530

    Responsive changes

--- src/pages/Home1.js
+++ src/pages/Home1.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HeadingAnimation3 from "../components/HeadingAnimation3";
+import "../assets/css/headingAnimation.css";
 const Home1 = () => {
   return (
     <div className=" align-self-center">
