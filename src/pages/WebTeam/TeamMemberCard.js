30733e090e725ba0e218a7cf41ca6ed34ec15098
karanlakhwani0910 <karan.lakhwani23@gmail.com>
2021-06-15 19:31:50 +0530

    Webteam modified

--- src/pages/WebTeam/TeamMemberCard.js
+++ src/pages/WebTeam/TeamMemberCard.js
@@ -2,15 +2,13 @@ import React from "react";
 
 const TeamMemberCard = (props) => {
   return (
-    <div className="col-lg-3 col-md-6 col-sm-12">
-      <div className="team-member">
+    <div className="team-member">
         <img alt="Team Member" src={props.image} />
         <h2>
           Jay Samson <br />
           <i className="fab fa-linkedin-in"></i>
         </h2>
       </div>
-    </div>
   );
 };
 
--- src/pages/WebTeam/WebTeam.js
+++ src/pages/WebTeam/WebTeam.js
@@ -2,29 +2,42 @@ import TeamMemberCard from "./TeamMemberCard";
 import Jay from "./Images/Jay.jpg";
 import "./WebTeam.css";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col, Container } from "react-bootstrap";
 
 const WebTeam = () => {
   return (
     <div className="web-team">
-      <Link to="/" >
-        <Button className='close-button' variant='light'>X</Button>
+      <Link to="/">
+        <Button className="close-button" variant="light">
+          X
+        </Button>
       </Link>
-      <br/><br/>
-      <div className="container-fluid">
-        <div className="row">
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-        </div>
-        <div className="row">
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-          <TeamMemberCard image={Jay} />
-        </div>
-      </div>
+      <br />
+      <br />
+      <Container fluid>
+        <Row>
+          <Col lg={4} sm={12}>
+            <TeamMemberCard image={Jay} />
+          </Col>
+          <Col lg={4} sm={12}>
+            <TeamMemberCard image={Jay} />
+          </Col>
+          <Col lg={4} sm={12}>
+            <TeamMemberCard image={Jay} />
+          </Col>
+
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={6} sm={12}>
+            <TeamMemberCard image={Jay} />
+          </Col>
+          <Col lg={6} sm={12}>
+            <TeamMemberCard image={Jay} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
