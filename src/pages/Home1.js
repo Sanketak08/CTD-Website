445fe0853f4fee839c97628b74fe6080259623a0
karanlakhwani0910 <karan.lakhwani23@gmail.com>
2021-06-19 01:08:00 +0530

    first commit of 19 june bc

--- src/pages/Home1.js
+++ src/pages/Home1.js
@@ -1,10 +1,27 @@
-import React from "react";
-import HeadingAnimation3 from "../components/HeadingAnimation3";
-const Home1 = () => {
-  return (
-    <div className=" align-self-center">
-      <HeadingAnimation3 />
+import React from 'react'
+import WAVES from 'vanta/dist/vanta.waves.min'
+import ctdlogo from '../assets/img/ctd.png'
+
+
+class Home1 extends React.Component {
+  constructor() {
+    super()
+    this.vantaRef = React.createRef()
+  }
+  componentDidMount() {
+    this.vantaEffect = WAVES({
+      el: this.vantaRef.current
+    })
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
+    return (
+    <div className='home-page' ref={this.vantaRef}>
+      <img src={ctdlogo} width='500px'  />
     </div>
-  );
-};
+    )
+  }
+}
 export default Home1;
