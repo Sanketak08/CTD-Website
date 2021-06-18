cb2c3c14daccad527f96590217553c4dea06e655
karanlakhwani0910 <karan.lakhwani23@gmail.com>
2021-06-18 21:13:22 +0530

    button added to navbar

--- src/pages/Navbar/NavigationBar.jsx
+++ src/pages/Navbar/NavigationBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import CTDlogo from "../../assets/img/ctd.png";
 import { Link } from "react-router-dom";
-import $ from "jquery";
 import "./Nav.css";
+import {isLogin} from '../../components/utils/index'
 
 const NavigationBar = () => {
   const [navbar, setNavbar] = React.useState(false);
@@ -18,15 +18,11 @@ const NavigationBar = () => {
 
   
 
-  $( '.navbar-nav .na-link' ).on( 'click', function () {
-    $( '.navbar-nav .na-link' ).find( '.na-link.active' ).removeClass( 'active' );
-    $( this ).parent( '.na-link' ).addClass( 'active' );
-  });
-
   window.addEventListener("scroll", changeBackground);
 
-  return (
-    <Navbar
+  if(isLogin()){
+    return(
+      <Navbar
       className={
         navbar ? "active nav-bar p1 sticky-top" : "nav-bar p1 sticky-top"
       }
@@ -53,16 +49,51 @@ const NavigationBar = () => {
           <Link className="na-link" to="ContactUs">
             Contact
           </Link>
-          <Link className="na-link" to="my-profile">
-            Profile
-          </Link>
-          <Link className="na-link" to="/login">
-            Login
-          </Link>
+          
+          <Link className="na-link" to="my-profile">Profile</Link>
         </Nav>
+        
       </Navbar.Collapse>
     </Navbar>
-  );
+    )
+  }
+
+    return(
+      <Navbar
+      className={
+        navbar ? "active nav-bar p1 sticky-top" : "nav-bar p1 sticky-top"
+      }
+      collapseOnSelect
+      expand="xl"
+    >
+      <Navbar.Brand>
+        <Link to="/">
+          <img alt="CTDLOGO" src={CTDlogo} className="logo" width="100px" />
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="m-auto">
+          <Link className="na-link" to="/">
+            Home
+          </Link>
+          <Link className="na-link" to="/AboutUs">
+            About
+          </Link>
+          <Link className="na-link" to="/events">
+            Events
+          </Link>
+          <Link className="na-link" to="ContactUs">
+            Contact
+          </Link>
+          
+        </Nav>
+        <Link className="na-link login" to="/login">
+          <Button size="lg">Login</Button>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>)
+  
 };
 
 export default NavigationBar;
