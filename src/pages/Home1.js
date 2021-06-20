71191fead348148222acd3bbf3ecdaa37dfae510
Sanket Kulkarni <sakulkarni0801@gmail.com>
2021-06-20 15:34:06 +0530

    Comments removed

--- src/pages/Home1.js
+++ src/pages/Home1.js
@@ -190,34 +190,6 @@ const Home1 = () => {
           </Row>
         </Container>
       </div>
-      {/* import React from 'react'
-import WAVES from 'vanta/dist/vanta.waves.min'
-import ctdlogo from '../assets/img/ctd.png' */}
-
-      {/* class Home1 extends React.Component {
-  constructor() {
-    super()
-    this.vantaRef = React.createRef()
-  }
-  componentDidMount() {
-    this.vantaEffect = WAVES({
-      el: this.vantaRef.current,
-      color: 0x343434,
-  shininess: 150.00,
-  
-    })
-  }
-  componentWillUnmount() {
-    if (this.vantaEffect) this.vantaEffect.destroy()
-  }
-  render() {
-    return (
-    <div className='home-page' ref={this.vantaRef}>
-      <img src={ctdlogo} width='500px'  />
-    </div>
-    )
-  }
-} */}
     </div>
   );
 };
