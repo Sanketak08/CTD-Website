c9a8144983acbfb8fd1f36810b8309eec24314f6
karanlakhwani0910 <karan.lakhwani23@gmail.com>
2021-06-21 11:54:39 +0530

    webteam page updated

--- src/pages/WebTeam/WebTeam.js
+++ src/pages/WebTeam/WebTeam.js
@@ -5,6 +5,9 @@ import Atharva from './Images/naphade.jpeg'
 import Neil from './Images/neil.jpeg'
 import Sanket from './Images/sanket.jpeg'
 import Pranav from './Images/pranav.jpeg'
+import Fateema from './Images/fateema.jpeg'
+import Omkar from './Images/omkar.jpeg'
+import Shreya from './Images/shreya.jpeg'
 import "./WebTeam.css";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, Container } from "react-bootstrap";
@@ -25,30 +28,32 @@ const WebTeam = () => {
             <TeamMemberCard linkedin='https://www.linkedin.com/in/atharva-naphade-540236148/'  name='Atharva Naphade' image={Atharva} />
           </Col>
           <Col lg={3} md={6} sm={12}>
+            <TeamMemberCard linkedin='https://www.linkedin.com/in/fatema-katawala-2b99451a1/'  name='Fatema Katawala' image={Fateema} />
+          </Col>
+          <Col lg={3} md={6} sm={12}>
             <TeamMemberCard linkedin='https://www.linkedin.com/in/karan-lakhwani' name="Karan Lakhwani" image={Karan} />
           </Col>
           <Col lg={3} md={6} sm={12}>
             <TeamMemberCard linkedin='https://www.linkedin.com/in/neil-deshpande-908aa118b/' name='Neil Deshpande' image={Neil} />
           </Col>
-          <Col lg={3} md={6} sm={12}>
-            <TeamMemberCard linkedin='https://www.linkedin.com/in/atharva-naphade-540236148/'  name='Atharva Naphade' image={Atharva} />
-          </Col>
+          
 
         </Row>
       </Container>
       <Container fluid>
         <Row>
           <Col lg={3} md={6} sm={12}>
-            <TeamMemberCard linkedin='https://www.linkedin.com/in/pranav-mohril/' name='Pranav Mohril' image={Pranav} />
+            <TeamMemberCard linkedin='https://www.linkedin.com/in/rakmo33/' name='Omkar Dabir' image={Omkar} />
           </Col>
-          <Col lg={3} md={6} sm={12}>
-            <TeamMemberCard linkedin='https://www.linkedin.com/in/sanket-a-kulkarni/' name='Sanket Kulkarni' image={Sanket} />
-          </Col>
+          
           <Col lg={3} md={6} sm={12}>
             <TeamMemberCard linkedin='https://www.linkedin.com/in/pranav-mohril/' name='Pranav Mohril' image={Pranav} />
           </Col>
           <Col lg={3} md={6} sm={12}>
             <TeamMemberCard linkedin='https://www.linkedin.com/in/sanket-a-kulkarni/' name='Sanket Kulkarni' image={Sanket} />
+          </Col>
+          <Col lg={3} md={6} sm={12}>
+            <TeamMemberCard linkedin='https://www.linkedin.com/in/shreya-deshpande-420297191/' name='Shreya Deshpande' image={Shreya} />
           </Col>
         </Row>
       </Container>
